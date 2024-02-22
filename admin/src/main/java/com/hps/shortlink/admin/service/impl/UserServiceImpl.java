c40edbbfafeda8d335579df76e566706ea5f20de
hu pingsheng <hu942658636@163.com>
2024-02-22 23:48:52 +0800

    布隆过滤器

--- admin/src/main/java/com/hps/shortlink/admin/service/impl/UserServiceImpl.java
+++ admin/src/main/java/com/hps/shortlink/admin/service/impl/UserServiceImpl.java
@@ -5,10 +5,13 @@ import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hps.shortlink.admin.common.convention.exception.ClientException;
 import com.hps.shortlink.admin.common.enums.UserErrorCodeEnum;
+import com.hps.shortlink.admin.config.RBloomFilterConfiguration;
 import com.hps.shortlink.admin.dao.entity.UserDO;
 import com.hps.shortlink.admin.dao.mapper.UserMapper;
 import com.hps.shortlink.admin.dto.resp.UserRespDTO;
 import com.hps.shortlink.admin.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.redisson.api.RBloomFilter;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
@@ -18,7 +21,11 @@ import org.springframework.stereotype.Service;
  * 用户接口实现层
  */
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements UserService {
+
+    private final RBloomFilter<String> userRegisterCachePenetrationBloomFilter;
+
     @Override
     public UserRespDTO getUserByUsername(String username) {
         LambdaQueryWrapper<UserDO> queryWrapper = Wrappers.lambdaQuery(UserDO.class)
@@ -32,9 +39,6 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
 
     @Override
     public Boolean hasUsername(String username) {
-        LambdaQueryWrapper<UserDO> queryWrapper = Wrappers.lambdaQuery(UserDO.class)
-                .eq(UserDO::getUsername, username);
-        UserDO userDO = baseMapper.selectOne(queryWrapper);
-        return userDO == null;
+        return userRegisterCachePenetrationBloomFilter.contains(username);
     }
 }
