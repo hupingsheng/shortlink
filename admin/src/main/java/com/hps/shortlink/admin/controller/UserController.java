c4a37788b8729c48a582216d60c7366435a73aa5
hu pingsheng <hu942658636@163.com>
2024-02-22 13:19:40 +0800

    业务全局异常拦截器

--- admin/src/main/java/com/hps/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/com/hps/shortlink/admin/controller/UserController.java
@@ -1,11 +1,10 @@
 package com.hps.shortlink.admin.controller;
 
 import com.hps.shortlink.admin.common.convention.result.Result;
-import com.hps.shortlink.admin.common.enums.UserErrorCodeEnum;
+import com.hps.shortlink.admin.common.convention.result.Results;
 import com.hps.shortlink.admin.dto.resp.UserRespDTO;
 import com.hps.shortlink.admin.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,9 +26,7 @@ public class UserController {
     @GetMapping("/api/shortlink/v1/user/{username}")
     public Result<UserRespDTO> getUserByUsername(@PathVariable("username") String username){
         UserRespDTO result = userService.getUserByUsername(username);
-
-        if (result == null)
-            return new Result<UserRespDTO>().setCode(UserErrorCodeEnum.USER_NULL.code()).setMessage(UserErrorCodeEnum.USER_NULL.message());
-        return new Result<UserRespDTO>().setCode("0").setData(result);
+        //controller只返回成功，不做判断处理
+        return Results.success(result);
     }
 }
--- admin/src/main/java/com/hps/shortlink/admin/service/impl/UserServiceImpl.java
+++ admin/src/main/java/com/hps/shortlink/admin/service/impl/UserServiceImpl.java
@@ -3,6 +3,8 @@ package com.hps.shortlink.admin.service.impl;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hps.shortlink.admin.common.convention.exception.ClientException;
+import com.hps.shortlink.admin.common.enums.UserErrorCodeEnum;
 import com.hps.shortlink.admin.dao.entity.UserDO;
 import com.hps.shortlink.admin.dao.mapper.UserMapper;
 import com.hps.shortlink.admin.dto.resp.UserRespDTO;
@@ -22,7 +24,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
         LambdaQueryWrapper<UserDO> queryWrapper = Wrappers.lambdaQuery(UserDO.class)
                 .eq(UserDO::getUsername, username);
         UserDO userDO = baseMapper.selectOne(queryWrapper);
-        if (userDO == null) return null;
+        if (userDO == null) throw new ClientException(UserErrorCodeEnum.USER_NULL);
         UserRespDTO result = new UserRespDTO();
         BeanUtils.copyProperties(userDO, result);
         return result;
